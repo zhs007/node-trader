137ac96d0d90311fe2ee98fd73e43da3fbd5be9e
zhs007 <sssxueren@gmail.com>
2018-02-18 21:08:11 +0800

    upd

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 "use strict";
 
-const { alignCandles } = require('./lib/candles');
-const { roundingDownTime, roundingUpTime, roundingOffTime } = require('./lib/time');
+const {
+    alignCandles,
+    scaleCandles
+} = require('./lib/candles');
+
+const {
+    roundingDownTime,
+    roundingUpTime,
+    roundingOffTime,
+    addTime
+} = require('./lib/time');
 
 // lst is like ['YYYY-MM-DD 11:00:00', 'YYYY-MM_DD 11:02:00']
 // output is like ['YYYY-MM-DD 11:00:00', 'YYYY-MM_DD 11:01:00', 'YYYY-MM_DD 11:02:00']
@@ -9,6 +18,14 @@ const { roundingDownTime, roundingUpTime, roundingOffTime } = require('./lib/tim
 // funcOff(strdatetime) return new strdatetime
 // [st, et)
 exports.alignCandles = alignCandles;
+
+// lst is align candles
+// options {realtime, ask_o, ask_c, ask_h, ask_l, bid_o, bid_c, bid_h, bid_l, volume}
+// srctime is like 1
+// srctimetype is like 'm'
+// desttime is like 4
+// desttimetype is like 'h'
+exports.scaleCandles = scaleCandles;
 
 // timetype is like y,M,d,h,m,s
 // if jsdate is 2018-02-08 01:00:00 & timetype is d
@@ -25,3 +42,8 @@ exports.roundingUpTime = roundingUpTime;
 // if jsdate is 2018-02-08 12:00:00 & timetype is d & rounding is 11
 //  then return 2018-02-09
 exports.roundingOffTime = roundingOffTime;
+
+// timevalue is like 4
+// time type is like 'h'
+// return jsdate
+exports.addTime = addTime;
